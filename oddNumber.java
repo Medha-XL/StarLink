a5d212a7ec187ae4ea9b3c71087bf46bf13bc6d1
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2021-07-20 07:52:41 +0530

    Add files via upload

---
+++ oddNumber.java
@@ -0,0 +1,20 @@
+package basicPrograms;
+
+public class oddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=0;
+		
+		while (i<=100) {
+			
+			if(i%2 != 0) {
+				System.out.println("odd number: " + i);
+			}
+			i++;
+		}
+
+	}
+
+}
