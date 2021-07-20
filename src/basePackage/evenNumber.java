d7bc428a8d40a125171f7a0ce170e2b1738f65a5
Ramprasad <ramprasadsuthi@gmail.com>
2021-07-20 07:47:31 +0530

    Create evenNumber.java
    This program will identify Even Numbers

---
+++ src/basePackage/evenNumber.java
@@ -0,0 +1,19 @@
+package basicPrograms;
+
+public class evenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for(i=0; i<=100; i++) {
+			
+			if(i%2 == 0) {
+				System.out.println("Even number : " + i);
+			}
+		}
+
+	}
+
+}
